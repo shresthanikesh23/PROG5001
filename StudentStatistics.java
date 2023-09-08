d3b5e3437cdf689b4be82a1510162a1ea149a6c0
shresthanikesh23 <shresthanikesh23@gmail.com>
2023-09-09 00:35:19 +1000

    Input the assignment name.

---
+++ StudentStatistics.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * This program allows the user to input the assignment name.
+ * @author (Nikesh)
+ * @version (02/09/2023)
+ */
+public class StudentStatistics
+{
+    public static void main(String[] args){
+    //F1: allows the user to input the assignment name
+    Scanner input = new Scanner (System.in);
+    System.out.println("Enter the Assignment Name: ");
+    String assignmentName = input.nextLine();
+    }
+}
