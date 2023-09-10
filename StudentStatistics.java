6da5191928dd65d1711e4b1c5622b524c11c9f2b
shresthanikesh23 <shresthanikesh23@gmail.com>
2023-09-10 20:03:53 +1000

    modified files to repository

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -8,9 +8,23 @@ import java.util.Scanner;
 public class StudentStatistics
 {
     public static void main(String[] args){
-    //F1: allows the user to input the assignment name
+    //F1: Allowing user to input the assignment name
     Scanner input = new Scanner (System.in);
     System.out.println("Enter the Assignment Name: ");
     String assignmentName = input.nextLine();
-    }
+    // F2: Allowing user to input students' marks
+    double[] marks = new double[5]; // Assuming there are 5 students
+    for (int i = 0; i < marks.length; i++) {
+         System.out.print("Enter mark for student " + (i + 1) + ": ");
+         double mark = input.nextDouble();
+         
+         // F3: Reporting and handling invalid input
+         if (mark < 0 || mark > 30) {
+                System.out.println("Invalid mark! Please enter a mark between 0 and 30.");
+                i--; // Repeating the current iteration to reenter the mark
+            } else {
+                marks[i] = mark;
+            }
+        }
+    }     
 }
